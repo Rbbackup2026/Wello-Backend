0d44539b1fc5135060b7cf9dac6b8e0d3a00c0a5
rachitrazobyte <rachit.razobyte@gmail.com>
2026-06-17 10:18:37 +0530

    NEW

--- Route/NotificationRoute.js
+++ Route/NotificationRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Notification = require("../models/Notification");
+const Notification = require("../Models/Notification");
 
 // Admin notification bhejta hai
 router.post("/send-user-notification", async (req, res) => {
